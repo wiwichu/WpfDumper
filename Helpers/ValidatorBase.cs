6de6d85e53faf9020e2d229549e873b149522d9b
wiwichu <mckeonppatrick@hotmail.com>
2016-10-02 21:36:02 -0400

    Validator

--- Helpers/ValidatorBase.cs
+++ Helpers/ValidatorBase.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace WpfDumper.Helpers
 {
@@ -32,7 +33,26 @@ namespace WpfDumper.Helpers
         protected override void SetProperty<T>(ref T member, T val, [CallerMemberName] string caller = "")
         {
             base.SetProperty(ref member, val, caller);
+            ValidateProperty(caller, val);
         }
 
+        private void ValidateProperty<T>(string caller, T val)
+        {
+            var results = new List<System.ComponentModel.DataAnnotations.ValidationResult>();
+            System.ComponentModel.DataAnnotations.ValidationContext context = 
+                new System.ComponentModel.DataAnnotations.ValidationContext(this);
+            context.MemberName = caller;
+            System.ComponentModel.DataAnnotations.Validator.TryValidateProperty(val, context, results);
+            if (results.Any())
+            {
+                errors[caller] = results.Select(c => c.ErrorMessage).ToList();
+            }
+            else
+            {
+                List<string> removed = null;
+                errors.TryRemove(caller, out removed);
+            }
+            ErrorsChanged(this, new DataErrorsChangedEventArgs(caller));
+        }
     }
 }
